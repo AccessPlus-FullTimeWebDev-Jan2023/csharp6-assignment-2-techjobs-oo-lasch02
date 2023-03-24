e8bf60b2aec18ca846b5ff8050aa41eb54f9e49d
Linda Schasch <lasch02@sbcglobal.net>
2023-03-24 14:12:58 -0500

    Task 3 - constructors complete; working on generators next.

--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,7 +3,7 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
+       
 
             public int Id { get; }
             private static int nextId = 1;
@@ -15,13 +15,29 @@ namespace TechJobsOOAutoGraded6
 
             // TODO: Task 3: Add the two necessary constructors.
 
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency)
+        {
+            Id = nextId;
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+
             // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
 
             // TODO: Task 5: Generate custom ToString() method.
                 //Until you create this method, you will not be able to print a job to the console.
 
 
-        */
+       
     }
 }
 
